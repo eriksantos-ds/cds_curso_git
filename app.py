e9b0e9b649476eb3bc47892896390a6d182d57b4
Erik Santos <erik.macedo@hotmail.com>
2024-10-27 14:43:13 -0300

    Arquivo app.py criado e inicializado
    Arquivo app.py criado e inicializado

---
+++ app.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+import streamlit as st
+
+def load_data():
+    return pd.read_csv('data/precessed/bikes_completes.csv')
+
+def main():
+    df = load_data()
+
+    st.dataframe(df)
+
+if__name__ == '__main__':
+    main()
